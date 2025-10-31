b4ef66737865f633871d2ba49570993b9be6b100
Adrian Salgado <adrians1209@hotmail.com>
2025-10-31 14:42:05 -0400

    update test

--- cards/Frontend/src/App.tsx
+++ cards/Frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Knights marketplace</h1>
+      <h1>Knights marketplace🔥</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
